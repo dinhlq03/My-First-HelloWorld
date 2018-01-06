ef1847323d855a00cc1c90a5a62e864796a85cc7
dinhlq03 <dinhlq03@gmail.com>
2018-01-06 16:40:23 +0700

    Create new method in the branch

--- src/org/hm/firstapp/Main.java
+++ src/org/hm/firstapp/Main.java
@@ -3,6 +3,12 @@ package org.hm.firstapp;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Le Quoc Dinh");
+
+        String myMessage = "Le Quoc Dinh";
+        System.out.println(showTheMessage(myMessage));
+    }
+
+    private static String showTheMessage(String message) {
+        return "Hello world and how are you " + message;
     }
 }
