74653a0e3a6b1e365827dadd832b5667b2535632
dinhlq03 <dinhlq03@gmail.com>
2018-01-06 17:09:30 +0700

    add new calculate method

--- src/org/hm/firstapp/Main.java
+++ src/org/hm/firstapp/Main.java
@@ -6,9 +6,16 @@ public class Main {
 
         String myMessage = "Le Quoc Dinh";
         System.out.println(showTheMessage(myMessage));
+
+        int a = 20, b = 54;
+        System.out.println(calculateMethod(a, b));
     }
 
     private static String showTheMessage(String message) {
         return "Hello world and how are you " + message;
+    }
+
+    private static int calculateMethod(int a, int b) {
+        return a + b + 10;
     }
 }
